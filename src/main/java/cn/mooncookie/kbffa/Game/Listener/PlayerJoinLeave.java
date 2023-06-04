fd04b1aeba8a851b316bcb6e242ed6777aa508ee
羽末 <yukiend@outlook.com>
2023-06-04 16:06:17 +0800

    Update PlayerJoinLeave.java

--- src/main/java/cn/mooncookie/kbffa/Game/Listener/PlayerJoinLeave.java
+++ src/main/java/cn/mooncookie/kbffa/Game/Listener/PlayerJoinLeave.java
@@ -23,10 +23,7 @@ public class PlayerJoinLeave implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
-        String name = player.getDisplayName();
         e.setJoinMessage(null);
-        String JoinFormat = LPRankProvider.getPrefix(e.getPlayer()) + name + LPRankProvider.getSuffix(e.getPlayer()) + "§6进入击退战场！";
-        String NonPermissionJoinFormat = "§7[§a+§7] " + LPRankProvider.getPrefix(e.getPlayer()) + name + LPRankProvider.getSuffix(e.getPlayer());
 
         player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 60, 1, false));
         player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 60, -100, false));
@@ -35,28 +32,12 @@ public class PlayerJoinLeave implements Listener {
         player.setGameMode(GameMode.ADVENTURE);
 
         Bukkit.getScheduler().runTaskLater(KnockBackFFA.getInstance(), () -> {
-            player.setGameMode(GameMode.SURVIVAL);
-            player.setHealth(20);
-            player.setFoodLevel(20);
-            player.getInventory().clear();
-            Bukkit.getScheduler().runTaskLater(KnockBackFFA.getInstance(), () -> GenShinImpact.giveItems(player), 1);
-            player.getActivePotionEffects().clear();
-            ScoreBoard.updateScoreboard(player);
-
-            if (!isVanished(player)) {
-                if (player.hasPermission("moclobby.joinmessage")) {
-                    Bukkit.broadcastMessage(JoinFormat);
-                } else {
-                    Bukkit.broadcastMessage(NonPermissionJoinFormat);
-                }
-            }
-        }, 60);
-
-        Bukkit.getScheduler().runTaskLater(KnockBackFFA.getInstance(), () -> {
             if (player.getLocation().getWorld().getName().equals("world")) {
                 MapChangeListener.currentMap.teleport(player);
             }
+            addPlayer(player);
         }, 60);
+
     }
 
     @EventHandler
@@ -67,6 +48,28 @@ public class PlayerJoinLeave implements Listener {
         e.setQuitMessage(NonPermissionQuitFormat);
         player.removeMetadata("lastBowTime", KnockBackFFA.getInstance());
         player.removeMetadata("lastJumpPadTime", KnockBackFFA.getInstance());
+    }
+
+    private void addPlayer(Player player) {
+        String name = player.getDisplayName();
+        String JoinFormat = LPRankProvider.getPrefix(player) + name + LPRankProvider.getSuffix(player) + "§6进入击退战场！";
+        String NonPermissionJoinFormat = "§7[§a+§7] " + LPRankProvider.getPrefix(player) + name + LPRankProvider.getSuffix(player);
+
+        player.setGameMode(GameMode.SURVIVAL);
+        player.setHealth(20);
+        player.setFoodLevel(20);
+        player.getInventory().clear();
+        Bukkit.getScheduler().runTaskLater(KnockBackFFA.getInstance(), () -> GenShinImpact.giveItems(player), 1);
+        player.getActivePotionEffects().clear();
+        ScoreBoard.updateScoreboard(player);
+
+        if (!isVanished(player)) {
+            if (player.hasPermission("moclobby.joinmessage")) {
+                Bukkit.broadcastMessage(JoinFormat);
+            } else {
+                Bukkit.broadcastMessage(NonPermissionJoinFormat);
+            }
+        }
     }
 
     private boolean isVanished(Player player) {
